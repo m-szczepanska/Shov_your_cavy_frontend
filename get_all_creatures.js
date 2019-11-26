7d5d341a95adc76a05c5d88729324ce2b0fddc9d
Maria Szczepańska <szczepanskamaria93@gmail.com>
2019-11-26 22:38:17 +0100

    Add fixes to 2 endpoints:
    	- add working pagination, upgrade css,
    	- add form for user settings page, upgrade css.

--- get_all_creatures.js
+++ get_all_creatures.js
@@ -1,61 +1,173 @@
 
 window.onload = function loadDoc() {
-    getResponse(
-        "http://localhost:8000/creatder/creatures/", "GET", "pig_target"
-    );
-};
+    var query_page = window.location.search
+    var user_id = localStorage.getItem("user_id");
+    var page = query_page.slice(6)
+    var url = `http://localhost:8000/creatder/creatures_all/${page}/`
+    var ident = "pig_target"
 
-function getResponse(url, method, ident) {
     var xhttp = new XMLHttpRequest();
-    // factor this bit out to a separate method
 
     xhttp.onreadystatechange = function() {
-        // readyState == 4 - response fully received
         if (this.readyState == 4 && this.status == 200) {
             var parsed = JSON.parse(this.response);
-            multiplyDiv(ident, parsed.length, parsed);
+            multiplyDiv(ident, parsed.objects.length, parsed.objects, user_id);
+            var pages_count = parsed["max_page"]
+            localStorage.setItem("pages_count", pages_count)
+            changePage(page, pages_count)
+            pagination_item("pagination_item", parsed, pages_count, page);
         }
     };
-
-    xhttp.open(method, url, true);
+    xhttp.open("GET", url, true);
     xhttp.send();
-    console.log(xhttp.responseText);
-    return xhttp.response;
 };
 
-function multiplyDiv(ident, count, json) {
+
+
+function multiplyDiv(ident, count, json, user_id) {
     console.log(json);
     var element = document.getElementById(ident);
-    for (var i=0; i<count; i++)
-        // element.innerHTML += `
-        //     <p class="inner">
-        //         Name: ${json[i]["name"]}<br>
-        //         Age: ${json[i]["age"]}<br>
-        //     </p>`;
+    for (var i=0; i<count; i++) {
+        console.log(json[i]["creature_card_photo"])
+      if (json[i]["creature_card_photo"] == null) {
+          var info_photo = "photo_info.png"
+      } else {
+          var info_photo = json[i]["creature_card_photo"]
+      }
+      if (json[i]["owner"]["id"] == user_id) {
+        console.log('dzial')
         element.innerHTML += `
-        <div class="col-md-3">
-            <div class="card mb-3 shadow-sm">
-                <img class="card-img-top" src="pictures/swin.jpg" alt="swinia", width="50%">
+        <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+                <img class="card-img-top" src="/Users/marsza/workspace/media/${info_photo}" alt="swinia" width="100%" height="250">
                 <div class="card-body">
                     <p class="card-text">Name: ${json[i]["name"]}</p>
                     <p class="card-text">Owner: ${json[i]["owner"]["login"]}</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            <button type="button" class="btn btn-sm btn-outline-secondary">Rate</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='upload_photo.html?id=${json[i]["id"]}'">View & upload photo</button>
                         </div>
                         <div class="rating-star">
                             <small class="text-muted">${json[i]["average_rating"]}</small>
-                            <img src="pictures/star.jpg" alt="" width="30" height="35">
+                            <img src="pictures/star.jpg" alt="" width="40" height="45">
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        `
-
+        </div>`
+    } else {
+      element.innerHTML += `
+      <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+              <img class="card-img-top" src="/Users/marsza/workspace/media/${info_photo}" alt="swinia" width="100%" height="250">
+              <div class="card-body">
+                  <p class="card-text">Name: ${json[i]["name"]}</p>
+                  <p class="card-text">Owner: ${json[i]["owner"]["login"]}</p>
+                  <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='rating.html?id=${json[i]["id"]}'">View & rate</button>
+                      </div>
+                      <div class="rating-star">
+                          <small class="text-muted">${json[i]["average_rating"]}</small>
+                          <img src="pictures/star.jpg" alt="" width="40" height="45">
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>`
+    }
+    };
 
 };
 
+function pagination_item(ident, json, pages_count, page) {
+    console.log(json.objects);
+    var element = document.getElementById(ident);
+    var page = Number(page)
 
-//onclick="loadDoc('http://localhost:8000/creatder/creatures/', myFunction)"
+    if (pages_count > 3 && pages_count - page <=2 && page >2){
+    element.innerHTML += `
+        <li class="page-item" class="p-2">
+        <a class="page-link" href="get_all_creatures.html?page=${pages_count-2}">${pages_count-2}
+        </a></li>
+        <li class="page-item" class="p-2">
+        <a class="page-link" href="get_all_creatures.html?page=${pages_count-1}">${pages_count-1}
+        </a></li>
+        <li class="page-item" class="p-2">
+        <a class="page-link" href="get_all_creatures.html?page=${pages_count}">${pages_count}
+        </a></li>`
+
+    } else if ((pages_count > 3 && page >=2)) {
+        element.innerHTML += `
+            <li class="page-item" class="p-2">
+            <a class="page-link" href="get_all_creatures.html?page=${page-1}">${page-1}
+            </a></li>
+            <li class="page-item" class="p-2">
+            <a class="page-link" href="get_all_creatures.html?page=${page}"><strong>${page}</strong>
+            </a></li>
+            <li class="page-item" class="p-2">
+            <a class="page-link" href="get_all_creatures.html?page=${page+1}">${page+1}
+            </a></li>`
+
+    // } else if (pages_count > 3 && page < 2) {
+    //     for (var i=1; i<(pages_count); i++) {
+    //         element.innerHTML += `
+    //         <li class="page-item" class="p-2">
+    //         <a class="page-link" href="get_all_creatures.html?page=${i}">${i}
+    //         </a></li>`
+    //     }
+    } else {
+        for (var i=1; i<(pages_count); i++) {
+            element.innerHTML += `
+            <li class="page-item" class="p-2">
+            <a class="page-link" href="get_all_creatures.html?page=${i}">${i}
+            </a></li>`
+        }
+    }
+};
+
+var current_page = window.location.search.slice(6);
+
+function prevPage() {
+    if (current_page > 1) {
+        current_page--;
+        window.location.href = `get_all_creatures.html?page=${current_page}`
+    }
+};
+
+function nextPage() {
+    var pages_count = localStorage.getItem("pages_count");
+    if (current_page < pages_count) {
+        current_page++;
+        window.location.href = `get_all_creatures.html?page=${current_page}`
+    }
+};
+
+function firstPage() {
+    window.location.href = "get_all_creatures.html?page=1"
+};
+
+function lastPage() {
+    var pages_count = localStorage.getItem("pages_count");
+    window.location.href = `get_all_creatures.html?page=${pages_count}`
+};
+
+function changePage(page_num, pages_count) {
+    var next = document.getElementById('btn_next');
+    var prev = document.getElementById('btn_prev');
+    // Validate page
+    if (page_num < 1) page_num = 1;
+    if (page_num > pages_count) page_num = pages_count;
+
+    if (page_num == 1) {
+        btn_prev.style.display = "none";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page_num == pages_count) {
+        btn_next.style.display = "none";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
--- user_settings.js
+++ user_settings.js
@@ -1,16 +1,16 @@
 window.onload = function getUserData() {
-    if (typeof(Storage) !== "undefined") {
-        var user_id = localStorage.getItem("user_id");
-        var token = localStorage.getItem("token");
+    if (localStorage.user_id) {
+        user_id = localStorage.getItem("user_id");
+        token = localStorage.getItem("token");
         getResponse(
-            `http://localhost:8000/creatder/users/${user_id}`, "GET", "user_data", "user_id", "token"
+            `http://localhost:8000/creatder/users/${user_id}`, "GET", "formAjax"
         );
     } else {
       window.location.href = "login.html"
     }
 }
 
-function getResponse(url, method, ident, user_id, token) {
+function getResponse(url, method, ident) {
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
@@ -19,31 +19,98 @@ function getResponse(url, method, ident, user_id, token) {
             var parsed = JSON.parse(this.response);
             innerHTMLresponse(parsed, ident);
         }
+        else if (this.readyState == 4 && this.status !== 200) {
+            // window.location.href = `error_page.html?=${this.status}`
+            console.log(this.response)
+        }
     };
 
     xhttp.open(method, url, true);
-    // var auth_header =user_id.
     xhttp.setRequestHeader("Authorization", `${user_id}:${token}`);
     xhttp.send();
     return xhttp.response;
+
 };
 
 function innerHTMLresponse(json, ident) {
     console.log(json);
     var element = document.getElementById(ident);
-      element.innerHTML += `
-        <div class="col align-items-center">
-            <div class="row align-items-center">
-                Name: ${json["name"]}
-            </div>
-            <div class="row align-items-center">
-                Login: ${json["login"]}
-            </div>
-            <div class="row align-items-center">
-                Email: ${json["email"]}
-            </div>
-            <div class="row align-items-center">
-                About myself: ${json["about_myself"]}
-            </div>
-        </div>`
+      // element.innerHTML += `
+      //   <div class="col-md-auto" id="col-info">
+      //       <div class="row justify-content-md-left">
+      //           <div class="col-md-auto py-2">
+      //               <strong>Name</strong>
+      //           </div>
+      //           <div class="col-md-auto py-2">
+      //               ${json["name"]}
+      //           </div>
+      //       </div>
+      //       <div class="row justify-content-md-left">
+      //           <div class="col-md-auto py-2">
+      //               <strong>Login</strong>
+      //           </div>
+      //           <div class="col-md-auto py-2">
+      //               ${json["login"]}
+      //           </div>
+      //       </div>
+      //       <div class="row justify-content-md-left">
+      //           <div class="col-md-auto py-2">
+      //               <strong>Email</strong>
+      //           </div>
+      //           <div class="col-md-auto py-2">
+      //               ${json["email"]}
+      //           </div>
+      //       </div>
+      //       <div class="row justify-content-md-left">
+      //           <div class="col-md-auto py-2">
+      //               <strong>About myself</strong>
+      //           </div>
+      //           <div class="col-md-auto py-2">
+      //               ${json["about_myself"]}
+      //           </div>
+      //       </div>
+      //   </div>`
+
+element.innerHTML +=
+` <label for="inputName" class="sr-only">Name</label>
+  <input type="name" id="inputName" class="form-control" value=${json["name"]} required>
+  <label for="inputLogin" class="sr-only">Login</label>
+  <input type="login" id="inputLogin" class="form-control" value=${json["login"]}  readonly>
+  <label for="inputEmail" class="sr-only">Email address</label>
+  <input type="email" id="inputEmail" class="form-control" value=${json["email"]} required autofocus>
+  <label for="about_myself" class="sr-only">About myself</label>
+
+  <textarea type="text" id="about_myself" class="form-control" name="text" rows="3" wrap="soft">${json["about_myself"]}</textarea>
+  <button class="btn btn-primary btn-block" id="register-btn" type="submit">Submit changes</button>`
 }
+
+// <input type="text" id="about_myself" class="form-control" size="150" maxlength="255" value="${json["about_myself"]}">
+document.querySelector('#formAjax').addEventListener("submit", function(e){
+  e.preventDefault();
+
+  var xhr = new XMLHttpRequest()
+  var user_id = localStorage.getItem("user_id");
+  var token = localStorage.getItem("token");
+
+  var url = `http://localhost:8000/creatder/users/${user_id}/`
+  var params = {
+      name: document.querySelector('#inputName').value,
+      email: document.querySelector('#inputEmail').value,
+      about_myself: document.querySelector('#about_myself').value,
+
+  };
+
+  xhr.open('PUT', url, true)
+  var auth_cred = `${user_id}:${token}`
+  xhr.setRequestHeader('Authorization', auth_cred)
+  xhr.setRequestHeader('Content-type', 'application/json')
+  xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4 && xhr.status == 201 || xhr.readyState == 4 && xhr.status == 200) {
+          alert('Your data has been changed');
+          console.log(xhr.responseText)
+          window.location.href = "file:///Users/marsza/workspace/zwierzu_front/user_settings.html"
+      }
+  }
+  xhr.send(JSON.stringify(params))
+
+});
